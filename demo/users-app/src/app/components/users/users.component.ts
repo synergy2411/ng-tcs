e6ed54d45e67d585e7ae65613532ec717160c47f
Sumit Khandelwal <sumitgniit@gmail.com>
2021-08-04 10:55:48 +0530

    code refactored

--- demo/users-app/src/app/components/users/users.component.ts
+++ demo/users-app/src/app/components/users/users.component.ts
@@ -1,25 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/model/user';
-
+import { USER_DATA } from '../../data/mock';
 @Component({
   selector : "app-users",
   templateUrl : "./users.component.html",
   styleUrls : [`./users.component.css`]
 })
 
-export class UsersComponent{
+export class UsersComponent implements OnInit{
   title = "The title"
   showUser = false;
-  user : User = {
-    firstName : "bill",
-    lastName : "gates",
-    income : 50000,
-    isWorking : true,
-    company : "Microsoft",
-    dob : new Date("Dec 18, 1965"),
-    image : "assets/images/bill.jpg",
-    vote : 120
+  user : User;
+
+  ngOnInit(){
+    this.user = USER_DATA;
   }
+
   onMoreInfo(usr : User){
     // console.log("Event Data [PARENT] ", eventdata);
     alert(`Mr. ${usr.lastName} is working with ${usr.company}`)
---
+++ demo/users-app/src/app/data/mock.ts
@@ -0,0 +1,12 @@
+import { User } from "../model/user";
+
+export const USER_DATA : User =  {
+  firstName : "bill",
+  lastName : "gates",
+  income : 50000,
+  isWorking : true,
+  company : "Microsoft",
+  dob : new Date("Dec 18, 1965"),
+  image : "assets/images/bill.jpg",
+  vote : 120
+}
