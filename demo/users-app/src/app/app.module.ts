3ad686a563a4b26b45fcb7882f7ec2a18d9e726a
Sumit Khandelwal <sumitgniit@gmail.com>
2021-08-05 13:28:30 +0530

    validation added

--- demo/users-app/src/app/app.module.ts
+++ demo/users-app/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
+
 import { AppComponent } from './app.component';
 import { UsersComponent } from './components/users/users.component';
 import { UserComponent } from './components/users/user/user.component';
@@ -23,7 +24,8 @@ import { RegisterComponent } from './components/auth/register/register.component
   ],
   imports: [        // Modules - Built-in | Custom
     BrowserModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],    // Services -
   bootstrap: [AppComponent]
--- demo/users-app/src/app/components/auth/register/register.component.ts
+++ demo/users-app/src/app/components/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators, ValidationErrors, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  username = new FormControl('',[
+    Validators.required,
+    Validators.email
+  ])
+  password = new FormControl('', [
+    Validators.required,
+    Validators.minLength(6),
+    this.exclamationMarkValidation
+  ])
+  registerForm : FormGroup
+
+  exclamationMarkValidation(control : AbstractControl): ValidationErrors | null{
+    const hasExclamation = control.value.indexOf("!") >= 0
+    return hasExclamation ? null : { hasExclamationMark : true };
+  }
+
+  constructor(private fb : FormBuilder) {
+    this.registerForm = this.fb.group({
+      username : this.username,
+      password : this.password
+    })
+   }
+
+   onRegister(){
+     console.log(this.registerForm);
+   }
+
+   onReset(){
+     this.registerForm.reset();
+   }
 
   ngOnInit(): void {
   }
